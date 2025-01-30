bbd3764a4a651b8445238b81c41e0936b2b153be
Ben Floyd <bfloydd@gmail.com>
2025-01-30 11:13:11 -0700

    Switch to file, dont open it

--- main.ts
+++ main.ts
@@ -88,9 +88,19 @@ export default class StreamsPlugin extends Plugin {
 			async () => {
 				const file = await createDailyNote(this.app, stream.folder);
 				if (file) {
-					const leaf = this.app.workspace.getLeaf(true);
-					await leaf.openFile(file);
-					this.app.workspace.revealLeaf(leaf);
+					// Check if file is already open in a tab
+					const existingLeaf = this.app.workspace.getLeavesOfType('markdown')
+						.find(leaf => (leaf.view as MarkdownView).file?.path === file.path);
+
+					if (existingLeaf) {
+						// Switch to existing tab
+						this.app.workspace.setActiveLeaf(existingLeaf, { focus: true });
+					} else {
+						// Open in new tab
+						const leaf = this.app.workspace.getLeaf('tab');
+						await leaf.openFile(file);
+						this.app.workspace.setActiveLeaf(leaf, { focus: true });
+					}
 				}
 			}
 		);
