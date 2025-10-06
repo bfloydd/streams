4e77adb85994afcb7e78ce3e30400ce1979f4e4c
Ben Floyd <bfloydd@gmail.com>
2025-10-06 14:24:28 -0600

    correct include

--- src/slices/file-operations/file-creation-strategies/index.ts
+++ src/slices/file-operations/file-creation-strategies/index.ts
@@ -1,3 +1,2 @@
-export type { FileCreationInterface } from './FileCreationStrategy';
 export { NormalFileStrategy } from './NormalFileStrategy';
 export { MeldEncryptedFileStrategy } from './MeldEncryptedFileStrategy';
